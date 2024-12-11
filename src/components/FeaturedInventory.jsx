4a12679f73c1291076cff41cda6afc06361d713c
Javed <jay7134@gmail.com>
2024-12-11 21:42:05 +0500

    inner pages

--- src/components/FeaturedInventory.jsx
+++ src/components/FeaturedInventory.jsx
@@ -10,24 +10,29 @@ const FeaturedInventory = ({title,desc})=>{
             contain: true,
             wrapAround: true,
             imagesLoaded: true,
+            autoPlay: 3000,
             prevNextButtons: false,
+            freeScroll: true,
             pageDots: true,
         });
         return () => flkty.destroy();
     },[]);
 
     return(
-        <section id="featured-inventory" className="relative bg-white py-[100px] overflow-hidden">
+        <section id="featured-inventory" className="relative bg-white py-[100px]">
             <div className="container">
                 <h2 className="text-[40px] leading-[48px] md:text-h2 md:leading-[62px] text-black font-[600] text-center">{ title ? title : "Featured inventory" }</h2>
                 { desc ? <p className="pt-[12px] text-normal text-[#475467] font-[400] text-center">{desc}</p> : '' }
                 
-                    <div className="featuredcarousel">
+                    
+                
+            </div>
+            <div className="container">
+                <div className="featuredcarousel pt-[50px] pb-[90px] w-full">
                         <div className="carousel-cell mr-4"><img src="../images/carousel/s-img1.png"/></div>
                         <div className="carousel-cell mr-4"><img src="../images/carousel/s-img2.png"/></div>
                         <div className="carousel-cell mr-4"><img src="../images/carousel/s-img3.png"/></div>
                     </div>
-                
             </div>
         </section>
     )
